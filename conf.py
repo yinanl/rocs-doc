c6c94d604f488ac90be7d6f7269be811663b23f3
yinanl <mmchong.yinan@gmail.com>
2021-07-08 14:32:27 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -33,6 +33,7 @@ release = '2.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+sys.path.append(os.path.abspath('_ext'))
 extensions = [
     'sphinx-prompt',
     'sphinx.ext.autodoc',
